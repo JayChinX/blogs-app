80563d99eb0f51cda94eb2fc6a40d8c569b50f66
qin <18339989537@163.com>
2024-03-28 23:12:31 +0800

    env

--- src/app/api/route.tsx
+++ src/app/api/route.tsx
@@ -1,17 +1,16 @@
 export const dynamic = 'force-dynamic' // defaults to force-static
 export async function GET(request: Request) {
-    // const res = await fetch('https://data.mongodb-api.com/...', {
-    //   headers: {
-    //         'Content-Type': 'application/json',
-          
-    //     // 'API-Key': process.env.DATA_API_KEY,
-    //   },
-    // })
-    // const data = await res.json()
+    const res = await fetch('https://data.mongodb-api.com/...', {
+        headers: {
+            'Content-Type': 'application/json',
+            'API-Key': process.env.DATA_API_KEY,
+        },
+    })
+    const data = await res.json()
 
-    const data = {
-        title: "123"
-    }
-  
+    // const data = {
+    //     title: "123"
+    // }
+
     return Response.json({ data })
-  }
+}
